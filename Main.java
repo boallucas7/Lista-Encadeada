68c82dfdd6ff7a9caaaa26f49bfaa9e0800c54f4
Lucas Freire <lucasfreiredesouza7@gmail.com>
2025-09-26 18:07:30 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        ListaEncadeada<String> l = new ListaEncadeada<>();
-        l.adiciona("Isabella");
-        l.adiciona("Guilherme");
-        l.adiciona("Aurora");
-        System.out.println(l.buscar("Guilherme"));
+        listaEncadeada<String> l = new listaEncadeada<>();
+        l.adiciona("Erica");
+        l.adiciona("Matheus");
+        l.adiciona("Lucas");
+        System.out.println(l.buscar("Lucas"));
     }
 }
